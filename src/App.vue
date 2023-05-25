9f54094a4a61b8efa2af17b734e2b65a53c09d21
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-05-25 17:28:54 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeader />
+  <div class="container">
+    <main>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/WhereView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheWhere from "@/components/TheWhere.vue";
+</script>
+
+<template>
+<main>
+  <TheWhere></TheWhere>
+</main>
+</template>
+
+<style scoped>
+
+</style>

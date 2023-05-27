050fdf288e1be3a81b1f8a78b49c65bda07bad35
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-05-27 14:59:52 +0200

    changing stuffs in the project

--- src/components/UtilityComponent/ArrowUp.vue
+++ src/components/UtilityComponent/ArrowUp.vue
@@ -1,12 +1,37 @@
-<script setup></script>
-
 <template>
   <div class="relative">
     <img
+      @click="scrollTop"
+      v-show="isShown"
       src="@/assets/img/logobastien-min.jpg"
       class="rounded-full h-10 w- auto z-50 fixed right-3 bottom-4"
+      alt="scroll top button"
     />
   </div>
 </template>
+
+<script setup>
+import { onBeforeMount, onMounted, ref } from 'vue'
+
+const isShown = ref(false)
+
+const scrollTop = function scrollTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+const checkScroll = function checkscroll() {
+  isShown.value = window.scrollY > 20
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', checkScroll)
+})
+onBeforeMount(() => {
+  window.removeEventListener('scroll', checkScroll)
+})
+</script>
 
 <style scoped></style>

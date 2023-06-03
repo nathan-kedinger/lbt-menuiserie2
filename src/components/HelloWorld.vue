6ea1e8cd29a7539dda842d560333589966cbe35c
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-06-03 18:21:29 +0200

    changing stuffs in the project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,8 +12,8 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a class="btn btn-success" href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <a class="btn btn-success" href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a>
+      + <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
 </template>

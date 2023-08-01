0670696631b70b70a1752fef7cfb67b6de2912ff
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-08-01 16:22:23 +0200

    Changing config to handle CORS

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': 'https://lbt-menuiserie.top'
+    }
   }
 })
